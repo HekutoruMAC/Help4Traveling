671b4ac607dee4a6ddff96b3418a14e9a3f50323
HekutoruMAC <hekutoru.mac@gmail.com>
2016-11-21 12:27:59 -0300

    Archivos por lote central.bat y web.bat para correr como administrador.

--- web.bat
+++ web.bat
@@ -1,4 +1,6 @@
 if not "%JAVA_HOME%" == "" goto ready
 for /d %%i in ("C:\Program Files\Java\jdk*") do setx JAVA_HOME "%%i" /M
 :ready
+%~d0
+cd %~dp0
 apache-ant-1.9.7\bin\ant -buildfile Help4TravelingWeb\build.xml
